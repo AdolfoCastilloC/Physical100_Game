99b6fd9f4ed5bbc94c6685c634fb4c46184847b3
adolfo salvador castillo cervantes <adolfo.castillo1502@gmail.com>
2025-11-28 20:54:21 -0600

    Adding Player Class file

---
+++ Player.py
@@ -0,0 +1,12 @@
+class Player:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+if __name__ == '__main__':
+    # Code to run
+    p1 = Player("Alejandra", 35)
+    print(p1.name)
+    print(p1.age)
+
